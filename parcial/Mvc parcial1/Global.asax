523d8df548a381a44d26d93a8ebb65ae899a20b4
Mayra Cabrera <mayracabrerajaldin@gmail.com>
2019-03-30 10:24:36 -0400

    commit 1

---
+++ parcial/Mvc parcial1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mvc_parcial1.MvcApplication" Language="C#" %>
---
+++ parcial/Mvc parcial1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc_parcial1.Startup))]
+namespace Mvc_parcial1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
